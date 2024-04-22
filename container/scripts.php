154a6e57bcea3139c6493514d2e13563d71ef1d7
kazimustafa95 <kazi.mustafa95@gmail.com>
2024-04-22 23:59:05 +0500

    first commit

---
+++ container/scripts.php
@@ -0,0 +1,11 @@
+<script src="assets/js/jquery_3.7.1.min.js"></script>
+
+<script src="assets/js/bootstrap_5.3.2.bundle.min.js"></script>
+
+<script src="assets/js/nouislider.min.js"></script>
+
+<script src="assets/js/aos-3.0.0.js"></script>
+
+<script src="assets/js/swiper10-bundle.min.js"></script>
+
+<script src="assets/js/shopus.js"></script>
