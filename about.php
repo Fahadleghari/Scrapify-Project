d320df6109173a602b177c512e6b9d72f5ae02b2
Ahmed Ali Mughal <ahmedali110d@gmail.com>
2024-04-25 20:08:45 +0500

    update home page and about page

---
+++ about.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="keywords"
+         content="ShopUS, bootstrap-5, bootstrap, sass, css, HTML Template, HTML,html, bootstrap template, free template, figma, web design, web development,front end, bootstrap datepicker, bootstrap timepicker, javascript, ecommerce template">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="icon" href="assets/images/homepage-one/icon.png">
+      <title>Shopus | About Us</title>
+      <?php include ('container/links.php') ?>
+   </head>
+   <body>
+      <?php include ('container/header.php') ?>
+      <!-- <div class="banner">
+         <div class="content">
+            <div class="left">
+               <h1>About Us</h1>
+            </div>
+            <div class="right">
+               <p>Discover the future of electronic waste <br> recycling with Scrapify,</p>
+            </div>
+         </div>
+      </div> -->
+
+
+      <section class="about hero-banner">
+         <div class="container">
+            <div class="about-section">
+               <div class="row align-items-center gy-5">
+                  
+                  <div class="col-lg-6">
+                     <div class="about-content" data-aos="fade-right">
+                        <h2 class="about-title">About Us</h2>
+                        
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="about-content" data-aos="fade-left">
+                        
+                        <h5 class="about-info">
+                        Discover the future of electronic waste <br> recycling with Scrapify,
+</h5>
+                       
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+
+
+      <section class="about">
+         <div class="container">
+            <div class="about-section">
+               <div class="row align-items-center gy-5">
+                  <div class="col-lg-6">
+                     <div class="about-img" data-aos="fade-right">
+                        <img src="assets/images/homepage-one/about/A-2.png" alt="img">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="about-content" data-aos="fade-up">
+                        <h3 class="about-title">Welcome to Scrapify</h3>
+                        <p class="about-info">
+                           Discover the future of electronic waste recycling with Scrapify, where we redefine the
+                           disposal of scrap electronics. Our mission is simple: prevent valuable materials from
+                           ending up in landfills and ensure our clients benefit financially from our cutting-edge
+                           recycling technology. Join us in our journey towards a greener planet and higher returns
+                           on your electronic scrap.
+                        </p>
+                        <div class="header-vendor-btn">
+                           <a href="contact-us.html" class="shop-btn">
+                              <span class="list-text shop-text">Contact Us</span>
+                              <span class="icon">
+                                 <svg width="24" height="16" viewBox="0 0 24 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                       d="M20.257 7.07205C20.038 7.07205 19.8474 7.07205 19.6563 7.07205C17.4825 7.07205 15.3086 7.07205 13.1352 7.07205C10.1545 7.07205 7.17336 7.0725 4.19265 7.0725C3.30392 7.0725 2.41519 7.07024 1.52646 7.07295C1.12124 7.07431 0.809811 7.25265 0.625785 7.62651C0.43866 8.00623 0.488204 8.37556 0.737704 8.70426C0.932347 8.96027 1.20529 9.08173 1.52867 9.08037C2.20948 9.07766 2.8903 9.07902 3.57111 9.07902C5.95285 9.07902 8.33415 9.07902 10.7159 9.07902C13.782 9.07902 16.8485 9.07902 19.9146 9.07902C20.0274 9.07902 20.1398 9.07902 20.2822 9.07902C20.1871 9.18332 20.1141 9.26865 20.0358 9.34857C19.5656 9.82672 19.0922 10.3022 18.6229 10.7812C18.1363 11.2779 17.6541 11.7791 17.1675 12.2757C16.4942 12.9634 15.8116 13.6415 15.1476 14.3391C14.9096 14.5893 14.8455 14.9157 14.9406 15.2575C15.156 16.0305 16.0567 16.2499 16.6119 15.6769C17.4342 14.8286 18.2655 13.9892 19.0927 13.1458C19.6948 12.5317 20.2968 11.9172 20.8985 11.3023C21.5952 10.5902 22.2911 9.87729 22.9878 9.1648C23.1059 9.04425 23.2249 8.9246 23.3435 8.8045C23.6903 8.45367 23.7239 7.84278 23.3943 7.4766C22.998 7.03683 22.5852 6.61241 22.1756 6.18573C21.7965 5.79066 21.4134 5.39965 21.0303 5.00909C20.6733 4.64473 20.3132 4.28306 19.9553 3.91915C19.6147 3.57284 19.2754 3.22563 18.9356 2.87887C18.5154 2.44948 18.0951 2.01964 17.6744 1.5907C17.2511 1.15861 16.8198 0.734188 16.4057 0.29261C16.0363 -0.101559 15.3697 -0.0816927 15.0344 0.257392C14.6238 0.672782 14.5999 1.26381 14.995 1.68552C15.3378 2.0517 15.6957 2.40342 16.0465 2.76192C16.929 3.66449 17.8111 4.56797 18.6937 5.47054C19.1829 5.97081 19.6735 6.47018 20.1632 6.97046C20.1885 6.99574 20.2123 7.02329 20.257 7.07205Z" />
+                                 </svg>
+                              </span>
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="about our-banner">
+         <div class="container">
+            <div class="about-section">
+               <div class="row align-items-center gy-5">
+                  <div class="col-lg-6">
+                     <div class="about-content" data-aos="fade-up">
+                        <h3 class="about-title">Our Mission</h3>
+                        <p class="about-info">
+                        At Scrapify, we are committed to an environmentally sustainable approach to electronic waste. By choosing us, you're not just disposing of your e-scrap; you're contributing to a global effort to reduce electronic waste. Our innovative technology allows us to offer you prices above the market rate for your electronic scrap, turning what was once waste into valuable resources.
+                        </p>
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="about-img" data-aos="fade-right">
+                        <img src="assets/images/homepage-one/about/A-3.png" alt="img">
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <section class="about">
+         <div class="container">
+            <div class="about-section">
+               <div class="row align-items-center gy-5">
+                  <div class="col-lg-6">
+                     <div class="about-img" data-aos="fade-right">
+                        <img src="assets/images/homepage-one/about/A-4.png" alt="img">
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="about-content" data-aos="fade-up">
+                        <h3 class="about-title">Why Scrapify?</h3>
+                        <p class="about-info">
+                           Choosing Scrapify means partnering with an environmentally conscious e-waste recycler that offers competitive prices for your scrap electronics. Our process is transparent and ensures you receive comprehensive documentation for your records. Trust us to handle your electronic waste with the care it deserves, ensuring it doesn't just avoid landfills but also brings you the best financial return.
+                        </p>
+                        <div class="header-vendor-btn">
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <?php include ('container/brands.php') ?> 
+      <?php include ('container/footer.php') ?>
+      <script src="assets/js/jquery_3.7.1.min.js"></script>
+      <script src="assets/js/bootstrap_5.3.2.bundle.min.js"></script>
+      <script src="assets/js/nouislider.min.js"></script>
+      <script src="assets/js/aos-3.0.0.js"></script>
+      <script src="assets/js/swiper10-bundle.min.js"></script>
+      <script src="assets/js/shopus.js"></script>
+   </body>
+</html>
